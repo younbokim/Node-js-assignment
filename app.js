668d90ac379f202d623d4d01b57b7650cb05145a
Sounwoo <mna800@naver.com>
2022-05-23 19:43:02 +0900

    과제

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const connect = require("./scheams");
+const app = express();
+const port = 5000;
+
+
+connect();
+
+
+const blogpostsRouter = require("./routes/blogposts");
+
+
+app.use(express.json());
+
+
+app.use("/api", [blogpostsRouter]);
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () =>{
+    console.log(port, "포트로 서버가 켜졌어요!");
+});
+
+
