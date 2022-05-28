3b718a76a1a07c1ce3922ec36c9b531088370263
Sounwoo <mna800@naver.com>
2022-05-28 11:15:31 +0900

    과제

---
+++ 입문/scheams/blogposts.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const blogpostsSchema = new mongoose.Schema({
+
+  // 게시글 제목
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  //작성자명
+  blogname: {
+    type: String,
+  },
+
+  // 작성글
+  comments: {
+    type: String,
+  },
+
+  // 작성일자
+  Date: {
+    type: Date,
+    default: Date.now,
+  },
+
+  // 비밀번호
+  password: {
+    type: Number,
+  },
+});
+
+module.exports = mongoose.model("Blogposts", blogpostsSchema);
